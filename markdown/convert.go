18ac6851b31fb56c3c575aa64bbd0d6db47d59ee
Zlatko Bratkovic <zbratkovic@haproxy.com>
2025-07-20 11:11:34 +0200

    add .center.flex option

--- markdown/convert.go
+++ markdown/convert.go
@@ -163,7 +163,13 @@ func prepare(md goldmark.Markdown, fileContent string) string {
 				CreateCleanMD(prepare(md, display)).String() + `</span>`
 		})
 	fileContent = processReplace(fileContent, ".center", ".center.end", func(data string) string {
-		return `<div style="text-align:center; justify-content: center; align-items: center;">` +
+		flex := ""
+		if strings.HasPrefix(data, ".flex") {
+			data = strings.TrimSpace(strings.TrimPrefix(data, ".flex"))
+			flex = "display: flex; "
+		}
+
+		return `<div style="text-align:center; ` + flex + `justify-content: center; align-items: center;">` +
 			CreateCleanMD(prepare(md, data)).String() + `</div>`
 	})
 	// fileContent = ProcessReplaceMiddle(fileContent, ".link{", "}(", ")", func(page, data string) string {
