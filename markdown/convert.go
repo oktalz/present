d98a0c822c09dd584f771d2921b448e65af1b8b1
Zlatko Bratkovic <zbratkovic@haproxy.com>
2025-07-20 10:39:07 +0200

    fix center command

--- markdown/convert.go
+++ markdown/convert.go
@@ -163,7 +163,7 @@ func prepare(md goldmark.Markdown, fileContent string) string {
 				CreateCleanMD(prepare(md, display)).String() + `</span>`
 		})
 	fileContent = processReplace(fileContent, ".center", ".center.end", func(data string) string {
-		return `<div style="text-align:center; display: flex; justify-content: center; align-items: center;">` +
+		return `<div style="text-align:center; justify-content: center; align-items: center;">` +
 			CreateCleanMD(prepare(md, data)).String() + `</div>`
 	})
 	// fileContent = ProcessReplaceMiddle(fileContent, ".link{", "}(", ")", func(page, data string) string {
